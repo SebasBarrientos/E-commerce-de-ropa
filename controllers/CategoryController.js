97d19dc28f767e0e2190d3dd68f8998a565ac578
Sebastian <sebas33696@gmail.com>
2024-05-02 13:55:26 +0200

    Listas tablas de categories

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,13 +1,77 @@
-const { Category } = require("../models/index");
+const { Category, Product, ProductsCategory } = require("../models/index");
 
 const CategoryController = {
-    async create(req,res){
+    async create(req, res) {
         try {
             const category = await Category.create(req.body)
-            res.status(201).send({msg:"Categoria creada con éxito",category})
+            res.status(201).send({ msg: "Categoria creada con éxito", category })
         } catch (error) {
             console.error(error);
             res.status(500).send(error)
+        }
+    }, async getAll(req, res) {
+        try {
+            const categories = await Category.findAll({
+                include: [{ model: Product, attributes: ["name"], through: { attributes: [] } }]
+            })
+            res.send({ msg: "Todas las categorias", categories })
+        } catch (error) {
+            console.error(error);
+            res.status(500).send(error);
+        }
+    },
+    async updateById(req, res) {
+        try {
+            await Category.update(req.body, {
+                where: {
+                    id: req.params.id,
+                },
+            });
+            res.send("Categoría actualizada con éxito");
+        } catch (error) {
+            console.error(error);
+            res.status(500).send(error);
+        }
+    },
+    async deleteById(req, res) {
+        try {
+            await Category.destroy({
+                where: {
+                    id: req.params.id,
+                },
+            });
+            await ProductsCategory.destroy({
+                where: {
+                    CategoryId: req.params.id,
+                },
+            });
+            res.send("Categoria eliminada con éxito");
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).send(error);
+        }
+    },
+    async getById(req, res) {
+        try {
+            const category = await Category.findByPk(req.params.id, { include: [{ model: Product, attributes: ["name"], through: { attributes: [] } }] });
+            res.send(category);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send(error);
+        }
+    },
+    async getByName(req, res) {
+        try {
+            const category = await Category.findAll({
+                where: {
+                    name: req.params.name,
+                }, include: [{ model: Product, attributes: ["name"], through: { attributes: [] } }]
+            });
+            res.send(category);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send(error);
         }
     }
 };
--- routes/categories.js
+++ routes/categories.js
@@ -4,5 +4,11 @@ const router = express.Router();
 
 
 router.post('/', CategoryController.create)
+router.get('/', CategoryController.getAll)
+router.put('/id/:id', CategoryController.updateById)
+router.delete('/id/:id', CategoryController.deleteById)
+router.get('/id/:id', CategoryController.getById)
+router.get('/name/:name', CategoryController.getByName)
+
 
 module.exports = router;
